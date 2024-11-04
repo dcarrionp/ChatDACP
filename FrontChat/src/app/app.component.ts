466f6b3e22058dd370ae048afcccac23a6633705
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-11-04 18:46:17 -0500

    Chat completo

--- FrontChat/src/app/app.component.ts
+++ FrontChat/src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'FrontChat';
+  private socket: any;
+
+  constructor(private renderer: Renderer2, private elRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.socket = io(); // Ensure your `socket.io` instance is initialized correctly
+
+    const messages = this.elRef.nativeElement.querySelector('#messages');
+    this.socket.on('chat message', (msg: string) => {
+      const item = this.renderer.createElement('li');
+      const text = this.renderer.createText(msg);
+      this.renderer.appendChild(item, text);
+      this.renderer.appendChild(messages, item);
+    });
+  }
 }
