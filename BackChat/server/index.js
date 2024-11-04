6f24474d2e57899090e5c52e72e2724e94ef2a08
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-11-04 14:15:31 -0500

    Front chat

--- BackChat/server/index.js
+++ BackChat/server/index.js
@@ -1,13 +1,22 @@
 import express from 'express'
+import logger from 'morgan'
+import path from 'path';
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
 
+const angularDistPath = path.join(process.cwd(), '../FrontChat/dist/front-chat/browser')
+app.use(express.static(angularDistPath))
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(angularDistPath, 'index.html'))
+})
+
+/*app.get('/', (req, res) => {
     res.send('<h1>Chat</h1>')
-}) 
+})*/
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
